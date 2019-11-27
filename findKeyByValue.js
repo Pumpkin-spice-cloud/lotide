f82bd1bdab4c3dcf2bbab0b54a23f75c0832fb68
Pumpkin-spice-cloud <alan_mo@live.com>
2019-11-27 21:37:22 +0000

    search through object and return name of property that contains desired result

---
+++ findKeyByValue.js
@@ -0,0 +1,50 @@
+const assertEqual = function(actual, expected) {
+  let check, checkBoolean, quoteMark1, quoteMark2, emoji;
+  if (typeof actual === 'string') {
+    quoteMark1 = '"';
+  } else {
+    quoteMark1 = '';
+  }
+  if (typeof expected === 'string') {
+    quoteMark2 = '"';
+  } else {
+    quoteMark2 = '';
+  }
+
+  if (actual === expected) {
+    check = 'Passed';
+    checkBoolean = '===';
+    emoji = '✅✅✅';
+   
+  } else {
+    check = 'Failed';
+    checkBoolean = '!==';
+    emoji = '⛔️⛔️⛔️';
+ 
+  }
+  
+  console.log(`${emoji} Assertion ${check}: ${quoteMark1}${actual}${quoteMark1} ${checkBoolean} ${quoteMark2 + expected + quoteMark2}`);
+};
+
+let findKeyByValue = function (showList, tvShow){
+  for (let show in showList){
+    console.log(showList[show]);
+    if (showList[show] === tvShow){
+      
+      return show;
+    }
+  }
+  return undefined;
+} 
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
