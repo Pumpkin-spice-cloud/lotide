bb5a8228c55ff898f90743f959605ce4122314c1
Pumpkin-spice-cloud <alan_mo@live.com>
2019-11-28 23:24:40 +0000

    find value in object

---
+++ findkey.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  let check, checkBoolean, quoteMark1, quoteMark2, emoji;
+  if (typeof actual === 'string') {
+    quoteMark1 = '"';
+  } else {
+    quoteMark1 = '';
+  }
+  if (typeof expected === 'string') {
+    quoteMark2 = '"';
+  } else {
+    quoteMark2 = '';
+  }
+
+  if (actual === expected) {
+    check = 'Passed';
+    checkBoolean = '===';
+    emoji = '✅✅✅';
+   
+  } else {
+    check = 'Failed';
+    checkBoolean = '!==';
+    emoji = '⛔️⛔️⛔️';
+ 
+  }
+  
+  console.log(`${emoji} Assertion ${check}: ${quoteMark1}${actual}${quoteMark1} ${checkBoolean} ${quoteMark2 + expected + quoteMark2}`);
+};
+
+
+
+let findkey = function(object, callback) {
+  let shell = Object.keys(object);
+  for (let i = 0; i < shell.length; i++) {
+
+    if (callback((object[shell[i]].stars))) {
+      return shell[i];
+    }
+  }
+};
+
+let testObject = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+
+};
+
+
+assertEqual((findkey(testObject, x => x === 2)), 'noma');
