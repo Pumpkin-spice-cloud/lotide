10e2dd7de48f4a63f71a3f75d53e33ebb8dfc31d
Pumpkin-spice-cloud <alan_mo@live.com>
2019-12-05 01:56:11 +0000

    refactored and drier

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let check, checkBoolean,  emoji;
-  if (actual === expected) {
-    check = 'Passed';
-    emoji = '✅✅✅';
-   
-  } else {
-    check = 'Failed';
-    emoji = '⛔️⛔️⛔️';
- 
-  }
-  (actual === true) ? checkBoolean = '===' : checkBoolean = '!==';
-
-  
-  
-  console.log(`${emoji} Assertion ${check}: [${input1}] ${checkBoolean} [${input2}]`);
-};
-let input1, input2; //I know global variables bad :/ but i don't want the return to be an array containing the input array
 let eqArrays = function(array1, array2) {
   let equiv = true;
   input1 = array1;
@@ -37,4 +19,4 @@ let eqArrays = function(array1, array2) {
   return equiv;
 };
 
-assertEqual(eqArrays(['1','3'], ['1','3']), true);
+module.exports = eqArrays;
