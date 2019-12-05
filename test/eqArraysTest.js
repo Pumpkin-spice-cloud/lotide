b9482df3a7f5a8854464758142c454b07230d116
Pumpkin-spice-cloud <alan_mo@live.com>
2019-12-05 01:59:04 +0000

    created test case and refactored code

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,3 +1,4 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 assertEqual(eqArrays(['1','3'], ['1','3']), true);
+assertEqual(eqArrays(['1','3',3], ['1','3',3]), true);
