4cc59319fc999fdd6351e6767f5fa10f681b220c
Pumpkin-spice-cloud <alan_mo@live.com>
2019-12-05 01:52:35 +0000

    refactored and made drier

--- tail.js
+++ tail.js
@@ -1,36 +1,6 @@
-const assertEqual = function(actual, expected) {
-  let check, checkBoolean, quoteMark1, quoteMark2, emoji;
-  if (typeof actual === 'string') {
-    quoteMark1 = '"';
-  } else {
-    quoteMark1 = '';
-  }
-  if (typeof expected === 'string') {
-    quoteMark2 = '"';
-  } else {
-    quoteMark2 = '';
-  }
-  
-  if (actual === expected) {
-    check = 'Passed';
-    checkBoolean = '===';
-    emoji = '✅✅✅';
-   
-  } else {
-    check = 'Failed';
-    checkBoolean = '!==';
-    emoji = '⛔️⛔️⛔️';
-  }
-    
-  console.log(`${emoji} Assertion ${check}: ${quoteMark1}${actual}${quoteMark1} ${checkBoolean} ${quoteMark2 + expected + quoteMark2}`);
-};
-
 let tail = function(array) {
   let output;
   output = (array && array.length) ?  array.slice(1) : [];
   return output;
 };
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
